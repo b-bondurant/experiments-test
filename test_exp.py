bed307ca4115437dbdb3eecae0fc9df642e444a6
Brad Bondurant <brad.bondurant@duke.edu>
2021-12-18 11:59:15 -0500

    print status dict

--- test_exp.py
+++ test_exp.py
@@ -8,7 +8,12 @@ print(foo.bar.__file__)
 
 
 class TestExp(EnvExperiment):
+    def build(self):
+        self.setattr_device('scheduler')
+        
     def run(self):
         # confirm locations at runtime
         print(foo.__file__)
         print(foo.bar.__file__)
+        # double check scheduler status dict
+        print(self.scheduler.get_status())
